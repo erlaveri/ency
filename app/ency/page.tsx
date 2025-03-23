23784aeb195ab284422e3e9448ecb4a7d21f4c8d
Karpov <erlaveri@gmail.com>
2025-03-23 14:05:59 +0200

    4

--- app/ency/page.tsx
+++ app/ency/page.tsx
@@ -1,15 +1,60 @@
 'use client'
 
-const items = [
-  {name: 'sdfsd'}
+import {useCallback} from "react";
+
+const Types = [
+  {name: 'Question', code: 'question'},
+  {name: 'Coding', code: 'coding'},
+  {name: 'Logic', code: 'logic'},
 ];
 
+const Languages = [
+  {name: 'JS', code: 'js'},
+  {name: 'TS', code: 'ts'},
+  {name: 'HTML', code: 'html'},
+  {name: 'CSS', code: 'css'},
+];
+
+const Categories = [
+  {name: 'General', code: 'general'},
+  {name: 'System Design', code: 'sysdes'},
+  {name: 'Algorithms', code: 'alg'},
+  {name: 'Patterns', code: 'patterns'},
+  {name: 'DOM', code: 'dom'},
+  {name: 'NodeJS', code: 'nodejs'},
+  {name: 'React', code: 'react'},
+];
+
+
 export default function EncyPage() {
+
+  const ss = useCallback(async () => {
+    console.log(window);
+  }, [])
+
   return (
     <div>
-      {items.map((item, index) => (
-        <div key={index}>{item.name}</div>
-      ))}
+      <div>
+        <select name="type">
+          {Types.map(item => (<option key={item.code} value={item.code}>{item.name}</option>))}
+        </select>
+      </div>
+      <hr className="mb-3"/>
+      <div>
+        <select name="languagies" multiple>
+          {Languages.map(item => (<option key={item.code} value={item.code}>{item.name}</option>))}
+        </select>
+      </div>
+      <hr className="mb-3"/>
+      <div>
+        <select name="categories" multiple>
+          {Categories.map(item => (<option key={item.code} value={item.code}>{item.name}</option>))}
+        </select>
+      </div>
+      <hr/>
+
+
+      <input type="button" value="Save"/>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   images: {unoptimized: true},
+  devIndicators: {position: "bottom-left"}
 };
 
 export default nextConfig;
