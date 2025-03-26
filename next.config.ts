ffb387e13b2d99dbfe00543e0589def4935569a3
Karpov <erlaveri@gmail.com>
2025-03-26 15:24:21 +0200

    add mantine

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   images: {unoptimized: true},
-  devIndicators: {position: "bottom-left"}
+  devIndicators: {position: "bottom-left"},
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 };
 
 export default nextConfig;
